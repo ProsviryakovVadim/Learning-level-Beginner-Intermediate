b9beda2aa092aad6000c54177aca13c4cfd64377
Vadim <vgprosviryakov@gmail.com>
2017-07-20 18:50:23 +0300

    Project 3/ Social Media
    Project 3 - Social Media

--- Project 3: Social Media/Project 3: Social Media/DetailViewController.swift
+++ Project 3: Social Media/Project 3: Social Media/DetailViewController.swift
@@ -7,32 +7,18 @@
 //
 
 import UIKit
-import Social
 
 class DetailViewController: UIViewController {
-    
+
     @IBOutlet weak var imageView: UIImageView!
     var selectedImage: String?
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
         title = selectedImage
         if let imageToLoad = selectedImage {
             imageView.image  = UIImage(named: imageToLoad)
         }
-    }
-    
-    @objc func shareTapped() {
-         let vc = UIActivityViewController(activityItems: [imageView.image!], applicationActivities: []) // In applicationActivities you can set VKApp and so on
-         vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
-         present(vc, animated: true)
-//        if let vc = SLComposeViewController(forServiceType: SLServiceTypeTwitter) {
-//            vc.setInitialText("Look at this great picture!")
-//            vc.add(imageView.image!)
-//            vc.add(URL(string: "http://www.photolib.noaa.gov/nssl"))
-//            present(vc, animated: true)
-//        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
---
+++ Project 3: Social Media/Project 3: Social Media/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Project 1: Storm Viewer
+//
+//  Created by Vadim on 7/20/17.
+//  Copyright © 2017 Vadim Prosviryakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Storm Viewer"
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        print(pictures)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+}
+
